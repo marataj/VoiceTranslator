e3740ee6a8b26d1d294c8de9fcc0773550593a55
Mati <ratajczak.mat@gmail.com>
2022-12-19 23:50:16 +0100

    changed the graphical layer of the interface

--- VoiceTranslator/translator/forms.py
+++ VoiceTranslator/translator/forms.py
@@ -7,9 +7,9 @@ languages_list=[(value, key) for key, value in available_languages.items()]
 class TranslateForm(forms.Form):
     source_language = forms.ChoiceField(choices=languages_list, widget=forms.Select(
         attrs={'class': 'form-control language-sel'}))
-    source_text = forms.CharField(max_length=200, widget=forms.Textarea(
-        attrs={'class': 'form-control text-area', 'cols': '40', 'rows': '8'}))
+    source_text = forms.CharField(max_length=200, required=False, widget=forms.Textarea(
+        attrs={'class': 'form-control text-area'}))
     target_language = forms.ChoiceField(choices=languages_list, widget=forms.Select(
         attrs={'class': 'form-control language-sel'}))
     target_text = forms.CharField(max_length=200, widget=forms.Textarea(
-        attrs={'class': 'form-control text-area', 'cols': '40', 'rows': '8'}), required=False)
+        attrs={'class': 'form-control text-area'}), required=False)
--- VoiceTranslator/translator/views.py
+++ VoiceTranslator/translator/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import TranslateForm
 from django.views.generic.base import View
 from deepl.translator_engine import Translator, TextTranslated
+from django.http import HttpRequest
 # Create your views here.
 
 
@@ -11,19 +12,26 @@ class TranslateView(View):
         return render(request, "translator/translator.html", {'form': TranslateForm()})
 
     def post(self, request):
-        print('#' * 30)
-        print(type(request.POST["source_text"]), 
-        request.POST["source_language"],
-        type(request.POST["target_language"]))
-        t = Translator("5cce4070-ebdd-5a51-373d-d2f094b4c760:fx")
-        result=t.translate(request.POST["source_text"], 
-        request.POST["source_language"],
-        request.POST["target_language"])
+        if "submit-btn" in request.POST:
+            t = Translator("5cce4070-ebdd-5a51-373d-d2f094b4c760:fx")
+            result=t.translate(request.POST["source_text"], 
+            request.POST["source_language"],
+            request.POST["target_language"])
 
-        form = TranslateForm(
-            initial={'source_language': result.source_language,
-            'source_text': result.source_text,
-            'target_language': result.target_language,
-            'target_text': result.translated_text})
+            form = TranslateForm(
+                initial={'source_language': result.source_language,
+                'source_text': result.source_text,
+                'target_language': result.target_language,
+                'target_text': result.translated_text})
+        
+        elif "swap-btn" in request.POST:
+            form = self.swap_langs(request)
 
         return render(request, "translator/translator.html", {'form': form})
+
+
+    def swap_langs(self, request: HttpRequest) -> TranslateForm:
+        return TranslateForm({'source_language': request.POST["target_language"],
+            'source_text': request.POST["source_text"],
+            'target_language': request.POST["source_language"],
+            'target_text': request.POST["target_text"]})
