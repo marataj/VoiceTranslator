4092c19aba545ef663c768b4b78d347f669a9577
Mati <ratajczak.mat@gmail.com>
2022-12-31 17:09:19 +0100

    add voice translate

--- VoiceTranslator/deepl/translator_engine.py
+++ VoiceTranslator/deepl/translator_engine.py
@@ -1,31 +1,34 @@
 import requests
 from enum import Enum
+import base64
+import speech_recognition as sr
+import os
+from dataclasses import dataclass
 
+__all__ = ["TextTranslated, Translator, SpeechRecognizer"]
 
 available_languages={
     "Polski": "PL",
     "English": "EN"
 }
 
-
+@dataclass
 class TextTranslated:
-    def __init__(self, source_text: str, source_language: str, target_language: str, translated_text: str) -> None:
-        self.source_text = source_text
-        self.source_language = source_language
-        self.target_language = target_language
-        self.translated_text = translated_text
+    source_text: str 
+    source_language: str
+    target_language: str 
+    translated_text: str
 
 
 class Translator:
-    def __init__(self, api_key: str ) -> None:
-        self.__api_key = api_key
-
-    def translate(self, source_text: str, source_language: str, target_language: str) -> TextTranslated:
-
+    API_KEY="5cce4070-ebdd-5a51-373d-d2f094b4c760:fx"
+    API_ENDPOINT = "https://api-free.deepl.com/v2/translate"
+    @classmethod
+    def translate(cls, source_text: str, source_language: str, target_language: str) -> TextTranslated:
         raw_result = requests.get(
-            "https://api-free.deepl.com/v2/translate",
+            cls.API_ENDPOINT,
             params={
-                "auth_key": self.__api_key,
+                "auth_key": cls.API_KEY,
                 "target_lang": target_language,
                 "text": source_text,
                 "source_lang": source_language
@@ -33,3 +36,31 @@ class Translator:
         )
 
         return TextTranslated(source_text, source_language, target_language, raw_result.json()['translations'][0]['text'])
+
+class SpeechRecognizer:
+    TEMP_PATH = "temp/temp.wav"
+    OUT_PATH = "temp/out.wav"
+
+    @classmethod
+    def analyze(cls, blob_base64: str, lang: str = "PL") -> str:
+        os.unlink(cls.TEMP_PATH)
+        os.unlink(cls.OUT_PATH)
+        blob_base64=blob_base64.split(",")[1]
+        blob=base64.decodebytes(bytes(blob_base64, 'UTF-8'))
+             
+        with open(cls.TEMP_PATH, "wb") as wav_file:
+            wav_file.write(blob)
+        
+        os.system(f"ffmpeg -i {cls.TEMP_PATH} {cls.OUT_PATH}")
+
+        r = sr.Recognizer()
+        with sr.AudioFile(cls.OUT_PATH) as source:
+            audio = r.record(source)
+        
+        try:
+            text = r.recognize_google(audio, language=lang)
+        except sr.UnknownValueError:
+            print("No speech recognized")
+            return ""
+                
+        return text
--- VoiceTranslator/translator/views.py
+++ VoiceTranslator/translator/views.py
@@ -1,33 +1,23 @@
 from django.shortcuts import render
 from .forms import TranslateForm
 from django.views.generic.base import View
-from deepl.translator_engine import Translator, TextTranslated
-from django.http import HttpRequest
-import base64
-import speech_recognition as sr
-import wave
-import librosa
-import soundfile
-from pathlib import Path
-import moviepy.editor as moviepy
-from pydub import AudioSegment
-import os
-import ffmpeg
+from deepl.translator_engine import Translator, SpeechRecognizer
 # Create your views here.
 
-
 class TranslateView(View):
-    #TEMP_DIR = Path(r"C:/temp")
-
+    
     def get(self, request):
         return render(request, "translator/translator.html", {'form': TranslateForm()})
 
     def post(self, request):
+        source_text = request.POST.get("source_text")
+        source_language = request.POST.get("source_language")
+        target_language = request.POST.get("target_language")
+        target_text = request.POST.get("target_text")
+        record = request.POST["record_field"]
+
         if "submit-btn" in request.POST:
-            t = Translator("5cce4070-ebdd-5a51-373d-d2f094b4c760:fx")
-            result=t.translate(request.POST["source_text"], 
-            request.POST["source_language"],
-            request.POST["target_language"])
+            result=Translator.translate(source_text, source_language, target_language)
 
             form = TranslateForm(
                 initial={'source_language': result.source_language,
@@ -36,50 +26,21 @@ class TranslateView(View):
                 'target_text': result.translated_text})
         
         elif "swap-btn" in request.POST:
-            form = self.swap_langs(request)
+            form = TranslateForm({'source_language': target_language,
+            'source_text': source_text,
+            'target_language': source_language,
+            'target_text': target_text})
+            
+        elif "record_field" in request.POST:
+            output = SpeechRecognizer.analyze(record, source_language)
+            result=Translator.translate(output,source_language, target_language)
+
+            form = TranslateForm(
+                initial={'source_language': result.source_language,
+                'source_text': result.source_text,
+                'target_language': result.target_language,
+                'target_text': result.translated_text})
+            
 
         return render(request, "translator/translator.html", {'form': form})
 
-
-    def swap_langs(self, request: HttpRequest) -> TranslateForm:
-        return TranslateForm({'source_language': request.POST["target_language"],
-            'source_text': request.POST["source_text"],
-            'target_language': request.POST["source_language"],
-            'target_text': request.POST["target_text"]})
-
-
-class VoiceRecordView(View):
-    
-    input_path = "temp.wav"
-    output_path = "out.wav"
-    def get(self, request):
-        return render(request, "translator/voice_rec.html", {'typ': "get"})
-
-    def post(self, request):
-        blob=request.POST["record_field"]
-        blob=blob.split(",")
-        blob=blob[1]
-        blob=base64.decodebytes(bytes(blob, 'UTF-8'))
-             
-        wav_file = open(self.input_path, "wb")
-        wav_file.write(blob)
-   
-        wav_file.close()
-        
-        os.system(f"ffmpeg -i {self.input_path} {self.output_path}")
-
-        r = sr.Recognizer()
-
-        hellow=sr.AudioFile(self.output_path)
-        with hellow as source:
-            audio = r.record(source)
-        try:
-            s = r.recognize_google(audio, language="pl-PL")
-            print("Text: "+s)
-        except Exception as e:
-            print("Exception: "+str(e))
-        
-        os.unlink(self.input_path)
-        os.unlink(self.output_path)
-        
-        return render(request, "translator/voice_rec.html", {'typ': "post"})
