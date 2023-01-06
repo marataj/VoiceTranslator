6517126940fc8fd008de8769b97a748375673f01
Mati <ratajczak.mat@gmail.com>
2023-01-06 20:01:15 +0100

    added users register feature

--- VoiceTranslator/translator/urls.py
+++ VoiceTranslator/translator/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.TranslateView.as_view())
+    path("", views.TranslateView.as_view(), name="home")
 ]
--- VoiceTranslator/users/views.py
+++ VoiceTranslator/users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
 from .forms import LoginForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
+from django.urls import reverse
 # Create your views here.
 
 class LoginUser(View):
@@ -21,12 +23,36 @@ class LoginUser(View):
         if user is not None:
             login(request, user)
             messages.success(request,"Login successful", extra_tags="success")
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(reverse("home"))
         # Redirect to a success page.
         
         else:
             messages.warning(request,"Wrong username or password", extra_tags="warning")
-            return HttpResponseRedirect("/users")
+            return HttpResponseRedirect(reverse("login"))
         # Return an 'invalid login' error message.
+    
+class LogoutUser(View):
+    def get(self, request):
+        logout(request)
+        messages.info(request,"User logged out")
+        return HttpResponseRedirect(reverse("home"))
+
+class RegisterUser(View):
+    def get(self, request):
+        return render(request, "register.html", {"form": UserCreationForm()})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.info(request,"Registered successfuly", extra_tags="success")
+            return HttpResponseRedirect(reverse("login"))
+        else:
+            messages.info(request,"Error", extra_tags="danger")
+
+    
+
+    
         
 
